c45093700ab7f62e77b78ad34401201b7347b42c
Shahmeer Khan <kshahmeer10@gmail.com>
2024-04-18 21:04:31 +0500

    cli base words counter

---
+++ index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tThis is Kshahmeer10 sentence words counter\n\t");
+
+const answers: { sentence: string } = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "please enter sentence for words count",
+  },
+]);
+
+const words = answers.sentence.trim().split(" ");
+
+//print your array of words to the concole
+console.log(words);
+//print count words
+console.log(`Your sentence words count is ${words.length}`);
